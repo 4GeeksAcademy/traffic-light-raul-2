f23cee8a23ee94273bcdcf72739f38444ba556ba
Raul M <118123543+pillow3@users.noreply.github.com>
2023-12-26 20:09:41 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const lights = [
+	{ id: "1", lightOff: "red-light", lightOn: "red-light red-active" },
+	{
+		id: "2",
+		lightOff: "yellow-light",
+		lightOn: "yellow-light yellow-active",
+	},
+	{
+		id: "3",
+		lightOff: "green-light",
+		lightOn: "green-light green-active",
+	},
+];
 
-//create your first component
-const Home = () => {
+function Home() {
+	const [selectedLight, setSelectedLight] = useState(null);
+
+	function handleClick(id) {
+		setSelectedLight(id !== selectedLight ? id : null);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			{lights.map((light) => (
+				<div
+					key={light.id}
+					onClick={() => {
+						handleClick(light.id);
+						console.log(light.id);
+					}}
+					className={
+						light.id === selectedLight
+							? light.lightOn
+							: light.lightOff
+					}></div>
+			))}
 		</div>
 	);
-};
+}
 
 export default Home;
